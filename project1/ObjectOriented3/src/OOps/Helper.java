d28fd8a39ac78a5e984b3e28e4508965ad258972
PayalKPI <payal.pitale@kpipartners.com>
2022-09-26 16:33:56 +0530

    commit1

---
+++ project1/ObjectOriented3/src/OOps/Helper.java
@@ -0,0 +1,31 @@
+package OOps;
+
+class Helper {
+//static keyword permits methods at class level:
+	//it is sharable across different classes
+	
+	
+	
+	
+	static int Multiply(int a, int b)
+	{
+		
+		
+		return a*b;
+	}
+	
+	
+	
+	
+	
+	
+	
+	static double Multiply(double a, double b)
+	{
+		
+		
+		return a * b;
+	}
+}
+
+
